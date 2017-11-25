4d91ac1312eb1718a962bb60429d11484fe750e3
Bhagyashree <bhagyashreeaher3@gmail.com>
2017-11-25 18:33:45 +0530

    updated expense claim

--- interaction/custom_methods.py
+++ interaction/custom_methods.py
@@ -28,3 +28,18 @@ def create_todo(owner, assigned_by, description, date,reference_name,reference_t
 	todo.reference_type = reference_type
 	todo.reference_name = reference_name
 	todo.insert(ignore_permissions=True)
+
+@frappe.whitelist()
+def add_expense_claim(doc):
+        doc_json=json.loads(doc)
+        emp = frappe.db.get_value("Employee",{"user_id":doc_json['responsible']},"name")
+        doc_json['employee'] = emp
+        """allow any logged user to post a comment"""
+        doc = frappe.get_doc(doc_json)
+
+        if doc.doctype != "Expense Claim":
+                frappe.throw(_("This method can only be used to create a Expense Claim"), frappe.PermissionError)
+
+        doc.insert(ignore_permissions = True)
+
+        return doc.as_dict()
